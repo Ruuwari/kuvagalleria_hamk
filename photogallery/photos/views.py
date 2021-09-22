27064cb881e9ed2ee1d828581b515a0753a3bee7
Ruuwari <juho20121@student.hamk.fi>
2021-09-22 22:03:30 +0300

    Authentication, Login & Register done

--- photogallery/photos/views.py
+++ photogallery/photos/views.py
@@ -1,7 +1,55 @@
 from django.shortcuts import render, redirect
 from .models import Category, Photo
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm
 # Create your views here.
 
+
+
+def loginUser(request):
+    page = 'login'
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('gallery')
+
+    return render(request, 'photos/login_register.html', {'page': page})
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+def registerUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+
+            user = authenticate(
+                request, username=user.username, password=request.POST['password1']) 
+
+            if user is not None:
+                login(request, user)
+                return redirect('gallery')
+
+
+
+    context = {'form': form, 'page': page}
+    return render(request, 'photos/login_register.html', context)
+
+
+@login_required(login_url='login')
 def gallery(request):
     category = request.GET.get('category')
     if category == None:
@@ -13,10 +61,12 @@ def gallery(request):
     context = {'categories': categories, 'photos': photos}
     return render(request, 'photos/gallery.html', context)
 
+@login_required(login_url='login')
 def viewPhoto(request, pk):
     photos = Photo.objects.get(id=pk)
     return render(request, 'photos/photo.html', {'photo': photos})
 
+@login_required(login_url='login')
 def addPhoto(request):
     categories = Category.objects.all()
 
